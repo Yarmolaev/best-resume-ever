b4067d3415f0b7fecc8ae53660bb3aaad4d83881
Yarmolaev <aleksejyarmolaev@gmail.com>
2017-10-05 00:14:48 +0200

    Initial yarmolaev template commit

--- src/resumes/resumes.js
+++ src/resumes/resumes.js
@@ -4,27 +4,28 @@ import '../resumes/left-right.vue';
 import '../resumes/oblique.vue';
 import '../resumes/side-bar.vue';
 import '../resumes/purple.vue';
+import '../resumes/yarmolaev.vue';
 
 import { PERSON } from '../person';
 import { terms } from '../terms';
 
 // Called by templates to decrease redundancy
-function getVueOptions (name) {
-  let opt = {
-    name: name,
-    data () {
-      return {
-        person: PERSON,
-        terms: terms
-      };
-    },
-    computed: {
-      lang () {
-        return this.terms[this.person.lang];
-      }
-    }
-  };
-  return opt;
+function getVueOptions(name) {
+    let opt = {
+        name: name,
+        data() {
+            return {
+                person: PERSON,
+                terms: terms
+            };
+        },
+        computed: {
+            lang() {
+                return this.terms[this.person.lang];
+            }
+        }
+    };
+    return opt;
 }
 
 export { getVueOptions };
